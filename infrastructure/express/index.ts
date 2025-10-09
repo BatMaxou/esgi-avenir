07c9185275de15e459913eb261bf1124d8663cef
BatMaxou <maximebatista.lycee@gmail.com>
2025-10-09 09:31:40 +0200

    :bug: Fix dotenv

--- infrastructure/express/index.ts
+++ infrastructure/express/index.ts
@@ -1,8 +1,5 @@
 import express from "express";
-import dotenv from "dotenv";
 
-dotenv.config();
-dotenv.config({ path: '.env.local', override: true })
 
 const startServer = async () => {
   const app = express();
--- infrastructure/express/utils/tools.ts
+++ infrastructure/express/utils/tools.ts
@@ -1,7 +1,12 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+dotenv.config({ path: '.env.local', override: true })
+
 export const databaseUser = process.env.DB_USER || '';
 export const databasePassword = process.env.DB_PASSWORD || '';
 export const databaseHost = process.env.DB_HOST || '';
 export const databaseName = process.env.DB_NAME || '';
 export const databasePort = process.env.DB_PORT || '';
-export const databaseUrl = `mysql://${databaseUser}:${databasePassword}@${databaseHost}:${databasePort}/${databaseName}?serverVersion=12.0.2-MariaDB&charset=utf8mb4
+export const databaseDsn = `mysql://${databaseUser}:${databasePassword}@${databaseHost}:${databasePort}/${databaseName}?serverVersion=12.0.2-MariaDB&charset=utf8mb4
 `;
