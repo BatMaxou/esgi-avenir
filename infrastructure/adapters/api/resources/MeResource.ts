16c47334bf556f3aa68045e0e1ad95c5fb4100c4
BatMaxou <maximebatista.lycee@gmail.com>
2025-12-02 17:56:45 +0100

    :bug: Auth fix - Forbidden

--- infrastructure/adapters/api/resources/MeResource.ts
+++ infrastructure/adapters/api/resources/MeResource.ts
@@ -1,5 +1,4 @@
 import { ApiClientInterface } from "../../../../application/services/api/ApiClientInterface";
-import { ApiClientError } from "../../../../application/services/api/ApiClientError";
 import { MeResourceInterface } from "../../../../application/services/api/resources/MeResourceInterface";
 import { paths } from "../../../../application/services/api/paths";
 import type { User } from "../../../../domain/entities/User";
