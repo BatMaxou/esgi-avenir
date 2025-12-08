5fb1ce142f8ddcc542d0cbc5fae61eda0cd3058c
DvK42 <guillaume.couvidou@live.fr>
2025-12-08 20:58:02 +0100

    ✨ Ajout des 3 nouveaux types d'opérations

--- infrastructure/frontend/src/components/ui/molecules/item/operation-account-item.tsx
+++ infrastructure/frontend/src/components/ui/molecules/item/operation-account-item.tsx
@@ -36,6 +36,12 @@ const OperationAccountItem = ({ operation }: OperationAccountItemProps) => {
       );
     case OperationEnum.INTEREST:
       return <InterestOperationAccountItem operation={operation} />;
+    case OperationEnum.FEE:
+      return <FeeOperationAccountItem operation={operation} />;
+    case OperationEnum.TO_BANK:
+      return <ToBankOperationAccountItem operation={operation} />;
+    case OperationEnum.FROM_BANK:
+      return <FromBankOperationAccountItem operation={operation} />;
     default:
       return null;
   }
@@ -60,7 +66,9 @@ const DepositOperationAccountItem = ({
           </Avatar>
         </ItemMedia>
         <ItemContent>
-          <span className="font-semibold text-md">Dépot</span>
+          <span className="font-semibold text-md">
+            {operation?.name ? operation.name : "Dépot"}
+          </span>
         </ItemContent>
         <ItemActions>
           <span className="font-medium text-lg text-green-600">
@@ -90,7 +98,9 @@ const WithdrawalOperationAccountItem = ({
           </Avatar>
         </ItemMedia>
         <ItemContent>
-          <span className="font-semibold text-md">Retrait</span>
+          <span className="font-semibold text-md">
+            {operation?.name ? operation.name : "Retrait"}
+          </span>
         </ItemContent>
         <ItemActions>
           <span className="font-medium text-lg text-red-600">
@@ -126,7 +136,9 @@ const TransferOperationAccountItem = ({
         </ItemMedia>
         <ItemContent>
           <span className="font-semibold text-md">
-            {operation.toId === account?.id
+            {operation?.name
+              ? operation.name
+              : operation.toId === account?.id
               ? "Virement entrant"
               : "Virement sortant"}
           </span>
@@ -165,7 +177,103 @@ const InterestOperationAccountItem = ({
           </Avatar>
         </ItemMedia>
         <ItemContent>
-          <span className="font-semibold text-md">Intérêts épargne</span>
+          <span className="font-semibold text-md">
+            {operation?.name ? operation.name : "Intérêts épargne"}
+          </span>
+        </ItemContent>
+        <ItemActions>
+          <span className="font-medium text-lg text-green-600">
+            +{operation.amount.toFixed(2)} €
+          </span>
+        </ItemActions>
+      </li>
+    </Item>
+  );
+};
+const FeeOperationAccountItem = ({ operation }: OperationAccountItemProps) => {
+  return (
+    <Item
+      className="p-4 border border-gray-100 bg-gray-50 rounded-lg shadow-md flex flex-row justify-between items-center"
+      asChild
+    >
+      <li>
+        <ItemMedia>
+          <Avatar className="size-10 bg-red-700 justify-center items-center ">
+            <AvatarImage
+              src="/assets/images/bank-fee-icon.svg"
+              className="w-6 h-6"
+            />
+            <AvatarFallback>fee</AvatarFallback>
+          </Avatar>
+        </ItemMedia>
+        <ItemContent>
+          <span className="font-semibold text-md">
+            {operation?.name ? operation.name : "Frais banquaire"}
+          </span>
+        </ItemContent>
+        <ItemActions>
+          <span className="font-medium text-lg text-red-600">
+            -{operation.amount.toFixed(2)} €
+          </span>
+        </ItemActions>
+      </li>
+    </Item>
+  );
+};
+const ToBankOperationAccountItem = ({
+  operation,
+}: OperationAccountItemProps) => {
+  return (
+    <Item
+      className="p-4 border border-gray-100 bg-gray-50 rounded-lg shadow-md flex flex-row justify-between items-center"
+      asChild
+    >
+      <li>
+        <ItemMedia>
+          <Avatar className="size-10 bg-red-700 justify-center items-center ">
+            <AvatarImage
+              src="/assets/images/bank-transfer-in-icon.svg"
+              className="w-6 h-6"
+            />
+            <AvatarFallback>to bank</AvatarFallback>
+          </Avatar>
+        </ItemMedia>
+        <ItemContent>
+          <span className="font-semibold text-md">
+            {operation?.name ? operation.name : "Virement vers la banque"}
+          </span>
+        </ItemContent>
+        <ItemActions>
+          <span className="font-medium text-lg text-red-600">
+            -{operation.amount.toFixed(2)} €
+          </span>
+        </ItemActions>
+      </li>
+    </Item>
+  );
+};
+const FromBankOperationAccountItem = ({
+  operation,
+}: OperationAccountItemProps) => {
+  return (
+    <Item
+      className="p-4 border border-gray-100 bg-gray-50 rounded-lg shadow-md flex flex-row justify-between items-center"
+      asChild
+    >
+      <li>
+        <ItemMedia>
+          <Avatar className="size-10 bg-green-700 justify-center items-center ">
+            <AvatarImage
+              src="/assets/images/bank-transfer-out-icon.svg"
+              className="w-6 h-6"
+            />
+            <AvatarFallback>from bank</AvatarFallback>
+          </Avatar>
+        </ItemMedia>
+        <ItemContent>
+          <span className="font-semibold text-md">
+            {operation?.name ? operation.name : "Virement depuis la banque"}
+          </span>
         </ItemContent>
         <ItemActions>
           <span className="font-medium text-lg text-green-600">
