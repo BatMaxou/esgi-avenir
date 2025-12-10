419bd8f0ca42415285251d9d8e6575eeeb8a161f
DvK42 <guillaume.couvidou@live.fr>
2025-12-10 20:29:23 +0100

    prettier

--- infrastructure/frontend/src/components/ui/molecules/cards/accounts-card.tsx
+++ infrastructure/frontend/src/components/ui/molecules/cards/accounts-card.tsx
@@ -9,7 +9,9 @@ export const AccountsCard = ({ count }: AccountsCardProps) => {
     <div className="h-48 w-full bg-gradient-to-br from-green-500 to-green-700 rounded-lg shadow-lg">
       <div className="h-full flex flex-col items-center justify-center">
         <div className="text-center">
-          <p className="text-white text-sm font-semibold mb-2">Comptes Clients</p>
+          <p className="text-white text-sm font-semibold mb-2">
+            Comptes Clients
+          </p>
           <h3 className="text-6xl font-bold text-white drop-shadow-lg">
             {count}
           </h3>
